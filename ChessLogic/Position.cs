9c2b1a49c5f3783a5a3c6c03c8b2e38e6eb3bd2b
Kulpot <kulpotwahu@gmail.com>
2024-03-15 02:22:41 +0800

    public class Position {     public int Row { get; }
        public int Column { get; }
    
        public Position(int row, int column)
        {
            Row = row;
            Column = column;
        }
    }

--- ChessLogic/Position.cs
+++ ChessLogic/Position.cs
@@ -8,6 +8,14 @@ namespace ChessLogic
 {
     public class Position
     {
+        public int Row { get; }
 
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }
