31d11e4831dcb1306984f8155b1ffafaf78edf1f
Kulpot <kulpotwahu@gmail.com>
2024-03-16 01:35:02 +0800

    Piece.cs -> namespace ChessLogic {     public abstract class Piece     {         public abstract PieceType Type { get; }
            public abstract Player Color { get; }
    
            public bool HasMoved { get; set; } = false;
    
            public abstract Piece Copy();
        }
    }

--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -8,6 +8,12 @@ namespace ChessLogic
 {
     public abstract class Piece
     {
+        public abstract PieceType Type { get; }
 
+        public abstract Player Color { get; }
+
+        public bool HasMoved { get; set; } = false;
+
+        public abstract Piece Copy();
     }
 }
