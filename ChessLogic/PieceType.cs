b39c187f76cfb1a636152634d88a3894c5af0c0c
Kulpot <kulpotwahu@gmail.com>
2024-03-16 01:29:38 +0800

    public enum PieceType  {      Pawn,      Bishop,      Knight,      Rook,      Queen,      King  }

--- ChessLogic/PieceType.cs
+++ ChessLogic/PieceType.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public enum PieceType
     {
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+        Queen,
+        King
     }
 }
