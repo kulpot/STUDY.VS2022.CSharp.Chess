85c3485b26bd991ee36fd09c7a3b0d39a831ea0b
Kulpot <kulpotwahu@gmail.com>
2024-03-16 02:28:30 +0800

    END-> pieces and The Board

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -9,6 +9,7 @@ namespace ChessLogic
     public class GameState
     {
         public Board Board { get; }
+
         public Player CurrentPlayer { get; private set; }
 
         public GameState(Player player, Board board)
