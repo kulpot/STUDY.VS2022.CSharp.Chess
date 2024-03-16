7e317aaad910c9da1d78cb4b5f9acdff37177638
Kulpot <kulpotwahu@gmail.com>
2024-03-17 05:22:46 +0800

    MainWindow.xaml.cs->
    using ChessLogic;
    
        public partial class MainWindow : Window
        {
            private readonly Image[,] piecImages = new Image[8, 8];
    
            public MainWindow()
            {
                InitializeComponent();
            }
    
            private void InitializeBoard()
            {
                for (int r = 0; r < 8; r++)
                {
                    for (int c = 0; c < 8; c++)
                    {
                        Image image = new Image();
                        piecImages[r, c] = image;
                        PieceGrid.Children.Add(image);
                    }
                }

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLogic;
 
 namespace ChessUI
 {
@@ -16,9 +17,24 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] piecImages = new Image[8, 8];
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    piecImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
         }
     }
 }
