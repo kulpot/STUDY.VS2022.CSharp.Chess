d1cd5edafbbf6cce76c03f591354876f79a8c6a0
Kulpot <kulpotwahu@gmail.com>
2024-03-17 05:27:49 +0800

    Images.cs-> using ChessLogic; public static class Images {     private static ImageSource LoadImage(string filePath)     {         return new BitmapImage(new Uri(filePath, UriKind.Relative));     } }

--- ChessUI/Images.cs
+++ ChessUI/Images.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using ChessLogic;
 
 namespace ChessUI
 {
     public static class Images
     {
-
+        private static ImageSource LoadImage(string filePath)
+        {
+            return new BitmapImage(new Uri(filePath, UriKind.Relative));
+        }
     }
 }
