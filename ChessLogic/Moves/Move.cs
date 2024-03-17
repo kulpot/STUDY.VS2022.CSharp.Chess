8a94f74e3590d0fa57966e7a90abe705ad4f4f24
Kulpot <kulpotwahu@gmail.com>
2024-03-18 01:32:39 +0800

    MoveFolder->add->Move.cs->

---
+++ ChessLogic/Moves/Move.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessLogic
+{
+    public abstract class Move
+    {
+        public abstract MoveType Type { get; }
+        public abstract Position FromPos { get; }
+        public abstract Position ToPos { get; }
+
+        public abstract void Execute(Board board);
+    }
+}
