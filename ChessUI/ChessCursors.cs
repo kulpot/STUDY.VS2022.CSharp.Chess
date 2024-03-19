36824fd086c582fd279ad2d212ad1dd7397ba3cc
Kulpot <kulpotwahu@gmail.com>
2024-03-20 05:37:47 +0800

    ChessCursors.cs->  public static readonly Cursor WhiteCursor = LoadCursor("Assets/CursorW.cur");  public static readonly Cursor BlackCursor = LoadCursor("Assets/CursorB.cur");

--- ChessUI/ChessCursors.cs
+++ ChessUI/ChessCursors.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -11,6 +7,9 @@ namespace ChessUI
 {
     public static class ChessCursors
     {
+        public static readonly Cursor WhiteCursor = LoadCursor("Assets/CursorW.cur");
+        public static readonly Cursor BlackCursor = LoadCursor("Assets/CursorB.cur");
+
         private static Cursor LoadCursor(string filePath)
         {
             Stream stream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative)).Stream;
