414d6119f6836ebcec40bb5b3c23f49819b2d1a5
Kulpot <kulpotwahu@gmail.com>
2024-03-20 05:35:35 +0800

    ChessCursors.cs->         private static Cursor LoadCursor(string filePath)

--- ChessUI/ChessCursors.cs
+++ ChessUI/ChessCursors.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ChessUI
 {
-    static class ChessCursors
+    public static class ChessCursors
     {
+        private static Cursor LoadCursor(string filePath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative)).Stream;
+            return new Cursor(stream, true);
+        }
     }
 }
