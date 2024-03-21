01ab87e7f016c8fd84952269d29fc433e78af30b
Kulpot <kulpotwahu@gmail.com>
2024-03-21 10:48:09 +0800

    Move.cs->         public virtual bool IsLegal(Board board)

--- ChessLogic/Moves/Move.cs
+++ ChessLogic/Moves/Move.cs
@@ -7,5 +7,13 @@
         public abstract Position ToPos { get; }
 
         public abstract void Execute(Board board);
+
+        public virtual bool IsLegal(Board board)
+        {
+            Player player = board[FromPos].Color;
+            Board boardCopy = board.Copy();
+            Execute(boardCopy);
+            return !boardCopy.IsInCheck(player);
+        }
     }
 }
