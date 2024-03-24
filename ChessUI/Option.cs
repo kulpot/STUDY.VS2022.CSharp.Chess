272bee0038cc3f51ae3f5584b6d218687bf86263
Kulpot <kulpotwahu@gmail.com>
2024-03-24 15:21:08 +0800

    Option.cs-> namespace ChessUI {     public enum Option     {         Restart,         Exit,         Continue     } }

--- ChessUI/Option.cs
+++ ChessUI/Option.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessUI
+﻿namespace ChessUI
 {
     public enum Option
     {
-
+        Restart,
+        Exit,
+        Continue
     }
 }
