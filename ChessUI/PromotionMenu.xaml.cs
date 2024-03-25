b47e9812d4fd00d93e2d8748ebd13076f91d09ac
Kulpot <kulpotwahu@gmail.com>
2024-03-25 19:35:11 +0800

    PromotionMenu.xaml.cs->         public event Action<PieceType> PieceSelected;

--- ChessUI/PromotionMenu.xaml.cs
+++ ChessUI/PromotionMenu.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using ChessLogic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessUI
 {
@@ -20,6 +9,8 @@ namespace ChessUI
     /// </summary>
     public partial class PromotionMenu : UserControl
     {
+        public event Action<PieceType> PieceSelected;
+
         public PromotionMenu()
         {
             InitializeComponent();
