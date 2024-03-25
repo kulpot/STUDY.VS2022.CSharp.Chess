0ddf25bc2351f41210e7353caeffe303e0a84592
Kulpot <kulpotwahu@gmail.com>
2024-03-25 19:09:15 +0800

    PawnPromotion.cs-> namespace ChessLogic {     public class PawnPromotion : Move     {         public override MoveType Type => MoveType.PawnPromotion;         public override Position FromPos { get; }         public override Position ToPos { get; }         private readonly PieceType newType;
            public PawnPromotion(Position from, Position to, PieceType newType)
            {
                FromPos = from;
                ToPos = to;
                this.newType = newType;
            }
    
            private Piece CreatePromotionPiece(Player color)
            {
                return newType switch
                {
                    PieceType.Knight => new Knight(color),
                    PieceType.Bishop => new Bishop(color),
                    PieceType.Rook => new Rook(color),
                    _ => new Queen(color)
                };
            }
        }
    }

--- ChessLogic/Moves/PawnPromotion.cs
+++ ChessLogic/Moves/PawnPromotion.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic.Moves
+﻿namespace ChessLogic
 {
-    internal class PawnPromotion
+    public class PawnPromotion : Move
     {
+        public override MoveType Type => MoveType.PawnPromotion;
+        public override Position FromPos { get; }
+        public override Position ToPos { get; }
+        private readonly PieceType newType;
+
+        public PawnPromotion(Position from, Position to, PieceType newType)
+        {
+            FromPos = from;
+            ToPos = to;
+            this.newType = newType;
+        }
+
+        private Piece CreatePromotionPiece(Player color)
+        {
+            return newType switch
+            {
+                PieceType.Knight => new Knight(color),
+                PieceType.Bishop => new Bishop(color),
+                PieceType.Rook => new Rook(color),
+                _ => new Queen(color)
+            };
+        }
     }
 }
