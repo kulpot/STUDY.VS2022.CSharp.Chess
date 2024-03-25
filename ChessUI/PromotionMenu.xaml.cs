ad9b5e938a7127bcf23ead8db3d31c478cb877fd
Kulpot <kulpotwahu@gmail.com>
2024-03-25 19:38:24 +0800

    PromotionMenu.xaml.cs-> using ChessLogic; using System.Windows.Controls; using System.Windows.Input;
    namespace ChessUI
    {
        /// <summary>
        /// Interaction logic for PromotionMenu.xaml
        /// </summary>
        public partial class PromotionMenu : UserControl
        {
            public event Action<PieceType> PieceSelected;
    
            public PromotionMenu(Player player)
            {
                InitializeComponent();
    
                QueenImg.Source = Images.GetImage(player, PieceType.Queen);
                BishopImg.Source = Images.GetImage(player, PieceType.Bishop);
                RookImg.Source = Images.GetImage(player, PieceType.Rook);
                KnightImg.Source = Images.GetImage(player, PieceType.Knight);
            }
    
            private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
            {
                PieceSelected?.Invoke(PieceType.Queen);
            }
    
            private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
            {
                PieceSelected?.Invoke(PieceType.Bishop);
            }
    
            private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
            {
                PieceSelected?.Invoke(PieceType.Rook);
            }
    
            private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
            {
                PieceSelected?.Invoke(PieceType.Knight);
            }
        }
    }

--- ChessUI/PromotionMenu.xaml.cs
+++ ChessUI/PromotionMenu.xaml.cs
@@ -11,29 +11,34 @@ namespace ChessUI
     {
         public event Action<PieceType> PieceSelected;
 
-        public PromotionMenu()
+        public PromotionMenu(Player player)
         {
             InitializeComponent();
+
+            QueenImg.Source = Images.GetImage(player, PieceType.Queen);
+            BishopImg.Source = Images.GetImage(player, PieceType.Bishop);
+            RookImg.Source = Images.GetImage(player, PieceType.Rook);
+            KnightImg.Source = Images.GetImage(player, PieceType.Knight);
         }
 
         private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            PieceSelected?.Invoke(PieceType.Queen);
         }
 
         private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            PieceSelected?.Invoke(PieceType.Bishop);
         }
 
         private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            PieceSelected?.Invoke(PieceType.Rook);
         }
 
         private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            PieceSelected?.Invoke(PieceType.Knight);
         }
     }
 }
