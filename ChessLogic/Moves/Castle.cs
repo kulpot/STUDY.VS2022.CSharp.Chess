28267a114d57bc4d1a521f1005d7d2c58078dcb4
Kulpot <kulpotwahu@gmail.com>
2024-03-26 15:06:29 +0800

    Castle.cs-> namespace ChessLogic {     public class Castle : Move     {         public override MoveType Type { get; }         public override Position FromPos { get; }         public override Position ToPos {  get; }
            private readonly Direction kingMoveDir;
            private readonly Position rookFromPos;
            private readonly Position rookToPos;
    
            public Castle(MoveType type, Position kingPos)
            {
                Type = type;
                FromPos = kingPos;
    
                if(type == MoveType.CastleKS)
                {
                    kingMoveDir = Direction.East;
                    ToPos = new Position(kingPos.Row, 6);
                    rookFromPos = new Position(kingPos.Row, 7);
                    rookToPos = new Position(kingPos.Row, 5);
                }
                else if(type == MoveType.CastleQS)
                {
                    kingMoveDir = Direction.West;
                    ToPos = new Position(kingPos.Row, 2);
                    rookFromPos = new Position(kingPos.Row, 0);
                    rookToPos = new Position(kingPos.Row, 3);
                }
            }
        }
    }

--- ChessLogic/Moves/Castle.cs
+++ ChessLogic/Moves/Castle.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChessLogic.Moves
+namespace ChessLogic
 {
-    internal class Castle
+    public class Castle : Move
     {
+        public override MoveType Type { get; }
+        public override Position FromPos { get; }
+        public override Position ToPos {  get; }
+
+        private readonly Direction kingMoveDir;
+        private readonly Position rookFromPos;
+        private readonly Position rookToPos;
+
+        public Castle(MoveType type, Position kingPos)
+        {
+            Type = type;
+            FromPos = kingPos;
+
+            if(type == MoveType.CastleKS)
+            {
+                kingMoveDir = Direction.East;
+                ToPos = new Position(kingPos.Row, 6);
+                rookFromPos = new Position(kingPos.Row, 7);
+                rookToPos = new Position(kingPos.Row, 5);
+            }
+            else if(type == MoveType.CastleQS)
+            {
+                kingMoveDir = Direction.West;
+                ToPos = new Position(kingPos.Row, 2);
+                rookFromPos = new Position(kingPos.Row, 0);
+                rookToPos = new Position(kingPos.Row, 3);
+            }
+        }
     }
 }
