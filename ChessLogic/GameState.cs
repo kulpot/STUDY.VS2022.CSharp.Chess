aeb897b92322d1cbac10b75d5375bd2bccc1e295
Kulpot <kulpotwahu@gmail.com>
2024-03-29 17:19:33 +0800

    GameState.cs-> private void CheckForGameOver() {     if(!AllLegalMovesFor(CurrentPlayer).Any())     {         if(Board.IsInCheck(CurrentPlayer))         {             Result = Result.Win(CurrentPlayer.Oppenent());         }         else         {             Result = Result.Draw(EndReason.Stalemate);         }     }     else if(Board.InsufficientMaterial())     {         Result = Result.Draw(EndReason.InsufficientMaterial);     } }

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -57,6 +57,10 @@
                     Result = Result.Draw(EndReason.Stalemate);
                 }
             }
+            else if(Board.InsufficientMaterial())
+            {
+                Result = Result.Draw(EndReason.InsufficientMaterial);
+            }
         }
 
         public bool IsGameOver()
