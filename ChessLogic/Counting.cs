3f6356aed295cae8cb8247504af9aba1c16bee9c
Kulpot <kulpotwahu@gmail.com>
2024-03-29 16:53:22 +0800

    Counting.cs-> public class Counting {     private readonly Dictionary<PieceType, int> whiteCount = new();     private readonly Dictionary<PieceType, int> blackCount = new();
        public int TotalCount { get; private set; }
    
        public Counting()
        {
            foreach(PieceType type in Enum.GetValues(typeof(PieceType)))
            {
                whiteCount[type] = 0;
                blackCount[type] = 0;
            }
        }
    }

--- ChessLogic/Counting.cs
+++ ChessLogic/Counting.cs
@@ -8,6 +8,18 @@ namespace ChessLogic
 {
     public class Counting
     {
+        private readonly Dictionary<PieceType, int> whiteCount = new();
+        private readonly Dictionary<PieceType, int> blackCount = new();
 
+        public int TotalCount { get; private set; }
+
+        public Counting()
+        {
+            foreach(PieceType type in Enum.GetValues(typeof(PieceType)))
+            {
+                whiteCount[type] = 0;
+                blackCount[type] = 0;
+            }
+        }
     }
 }
