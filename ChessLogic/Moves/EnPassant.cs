ae2796276c7923742d8497db758100e71ba3e207
Kulpot <kulpotwahu@gmail.com>
2024-03-30 16:02:36 +0800

    EnPassant.cs-> //public override void Execute(Board board) public override bool Execute(Board board) {     new NormalMove(FromPos, ToPos).Execute(board);     board[capturePos] = null;
        return true;
    }

--- ChessLogic/Moves/EnPassant.cs
+++ ChessLogic/Moves/EnPassant.cs
@@ -15,10 +15,13 @@
             capturePos = new Position(from.Row, to.Column);
         }
 
-        public override void Execute(Board board)
+        //public override void Execute(Board board)
+        public override bool Execute(Board board)
         {
             new NormalMove(FromPos, ToPos).Execute(board);
             board[capturePos] = null;
+
+            return true;
         }
     }
 }
