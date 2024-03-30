50e51e3f4dd799d15a0fa2c0b55691d4986fcb87
Kulpot <kulpotwahu@gmail.com>
2024-03-30 15:57:37 +0800

    NormalMove.cs-> //public override void Execute(Board board) public override bool Execute(Board board) {     Piece piece = board[FromPos];     bool capture = !board.IsEmpty(ToPos);     board[ToPos] = piece;     board[FromPos] = null;     piece.HasMoved = true;
        return capture || piece.Type == PieceType.Pawn;
    }

--- ChessLogic/Moves/NormalMove.cs
+++ ChessLogic/Moves/NormalMove.cs
@@ -18,12 +18,16 @@ namespace ChessLogic
             ToPos = to;
         }
 
-        public override void Execute(Board board)
+        //public override void Execute(Board board)
+        public override bool Execute(Board board)
         {
             Piece piece = board[FromPos];
+            bool capture = !board.IsEmpty(ToPos);
             board[ToPos] = piece;
             board[FromPos] = null;
             piece.HasMoved = true;
+
+            return capture || piece.Type == PieceType.Pawn;
         }
     }
 }
