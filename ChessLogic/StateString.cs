8f6839531ed826a72560d6dfca9156a807656dab
Kulpot <kulpotwahu@gmail.com>
2024-04-02 13:38:04 +0800

    StateString.cs->         private void AddCurrentPlayer(Player currentPlayer)

--- ChessLogic/StateString.cs
+++ ChessLogic/StateString.cs
@@ -82,5 +82,10 @@ namespace ChessLogic
                 AddRowData(board, r);
             }
         }
+
+        private void AddCurrentPlayer(Player currentPlayer)
+        {
+
+        }
     }
 }
